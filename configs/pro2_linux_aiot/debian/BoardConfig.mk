a42232d72eca869750e52e6a733b3f2bb19f4edf
gerrit-sdk <gerrit-sdk@allwinnertech.com>
2024-08-06 15:44:52 +0800

    aiot-linux-v1.2 release

--- configs/pro2_linux_aiot/debian/BoardConfig.mk
+++ configs/pro2_linux_aiot/debian/BoardConfig.mk
@@ -2,7 +2,7 @@ LICHEE_CHIP:=sun55iw3p1
 LICHEE_ARCH:=arm64
 LICHEE_BRANDY_VER:=2.0
 LICHEE_BRANDY_DEFCONF:=sun55iw3p1_defconfig
-LICHEE_ROOTFS=must-do-by-yourself-in-dir-device-config-rootfs_tar
+LICHEE_ROOTFS=ubuntu-base-22.04.3-base-arm64.tar.bz2
 
 ifeq ($(BSP_BUILD_WITH_CLANG),true)
 	ANDROID_CLANG_PATH  := prebuilts/clang/host/linux-x86/clang-r450784d/bin
