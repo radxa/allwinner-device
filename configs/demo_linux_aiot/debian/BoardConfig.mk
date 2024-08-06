a96689ab3984156781620d6d7c87f75414f7ad1d
gerrit-sdk <gerrit-sdk@allwinnertech.com>
2024-08-06 15:44:50 +0800

    aiot-linux-v1.2 release

--- configs/demo_linux_aiot/debian/BoardConfig.mk
+++ configs/demo_linux_aiot/debian/BoardConfig.mk
@@ -1,3 +1,3 @@
 LICHEE_BRANDY_DEFCONF:=sun55iw3p1_t527_defconfig
 LICHEE_KERN_VER:=
-LICHEE_ROOTFS=must-do-by-yourself-in-dir-device-config-rootfs_tar
+LICHEE_ROOTFS=ubuntu-base-22.04.3-base-arm64.tar.bz2
