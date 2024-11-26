ce887e1881fcd02ef899eecaa744e2014d97b5f0
gerrit-sdk <gerrit-sdk@allwinnertech.com>
2024-11-26 18:17:59 +0800

    aiot-linux-v1.3 release

--- configs/default/autobuild.mk
+++
@@ -1,6 +0,0 @@
-ifeq ($(LICHEE_PLATFORM),android)
-LICHEE_BOARD:=pro2
-else
-LICHEE_BOARD:=pro2
-endif
-KERNEL_BRANCH:=sunxi-dev-aosp
--- configs/pro2_linux_aiot/debian/BoardConfig.mk
+++ configs/pro2_linux_aiot/debian/BoardConfig.mk
@@ -2,7 +2,7 @@ LICHEE_CHIP:=sun55iw3p1
 LICHEE_ARCH:=arm64
 LICHEE_BRANDY_VER:=2.0
 LICHEE_BRANDY_DEFCONF:=sun55iw3p1_defconfig
-LICHEE_ROOTFS=ubuntu-base-22.04.3-base-arm64.tar.bz2
+LICHEE_ROOTFS=
 
 ifeq ($(BSP_BUILD_WITH_CLANG),true)
 	ANDROID_CLANG_PATH  := prebuilts/clang/host/linux-x86/clang-r450784d/bin
